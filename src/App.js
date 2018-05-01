50a0134f7dc4a43942f32a3478c59a5fdb8a2f9d
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-05-01 16:57:38 -0700

    Implement conversion functions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from "react";
 
+// Conversion Functions
+function toCelsius(fahrenheit) {
+  return (fahrenheit - 32) * 5 / 9;
+}
+
+function toFahrenheit(celsius) {
+  return celsius * 9 / 5 + 32;
+}
+
+function tryConvert(temperature, convert) {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) {
+    return "";
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
+
 const scaleNames = {
   c: "Celsius",
   f: "Fahrenheit"
 };
 
+// Components
 function BoilingVerdict(props) {
   return props.celsius >= 100 ? (
     <p>The water would boil.</p>
