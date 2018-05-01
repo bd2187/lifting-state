6e7779f20baf34e592f2c6d7a704b05a36856fe0
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-05-01 16:43:23 -0700

    Create BoilingVerdict and Calculator component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
 import React, { Component } from "react";
 
+function BoilingVerdict(props) {
+  return props.celsius >= 100 ? (
+    <p>The water would boil.</p>
+  ) : (
+    <p>The water would not boil.</p>
+  );
+}
+
+class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = { temperature: "" };
+  }
+
+  handleChange(e) {
+    this.setState({ temperature: e.target.value });
+  }
+
+  render() {
+    const temperature = this.state.temperature;
+    return (
+      <fieldset>
+        <legend>Enter temperature in Celsius:</legend>
+        <input value={temperature} onChange={this.handleChange} />
+        <BoilingVerdict celsius={parseFloat(temperature)} />
+      </fieldset>
+    );
+  }
+}
 class App extends Component {
   render() {
-    return <h1>Test</h1>;
+    return <Calculator />;
   }
 }
 
